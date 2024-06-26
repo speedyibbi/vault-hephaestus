53cfde75c73700fd87d0b9e1942d8722f6df539b
speedyibbi <ibbi2k2.khan@gmail.com>
2024-06-26 17:22:34 +0500

    build: :heavy_plus_sign: install and configure tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	purge: [],
+	darkMode: false,
+	content: ['./src/frontend/**/*.{html,js,jsx,ts,tsx}'],
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};

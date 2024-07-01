eef1075ca8fa578a7d536a983cedf9309c2e1602
speedyibbi <ibbi2k2.khan@gmail.com>
2024-07-02 02:13:00 +0500

    feat: :sparkles: add image cropper

--- src/frontend/components/file-image-preview-input.tsx
+++ src/frontend/components/file-image-preview-input.tsx
@@ -1,7 +1,16 @@
-import { useState } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
+import { useEffect, useRef, useState } from 'react';
+import ReactCrop, {
+	centerCrop,
+	convertToPixelCrop,
+	makeAspectCrop,
+	type Crop,
+} from 'react-image-crop';
 
 import FileInput from './file-input';
+
+import { setCanvasPreview } from '../utils/helpers';
+
+import 'react-image-crop/dist/ReactCrop.css';
 
 interface Props {
 	id?: string;
@@ -12,7 +21,12 @@ export default function FileImagePreviewInput({
 	id = 'image',
 	name = 'image',
 }: Props) {
+	const imageRef = useRef<HTMLImageElement | null>(null);
+	const cropperRef = useRef<HTMLDialogElement | null>(null);
+	const canvasRef = useRef<HTMLCanvasElement | null>(null);
 	const [imagePreview, setImagePreview] = useState('');
+	const [imageCropped, setImageCropped] = useState(false);
+	const [crop, setCrop] = useState<Crop>();
 
 	const imageSelectionHandler = (
 		event: React.ChangeEvent<HTMLInputElement>
@@ -22,11 +36,43 @@ export default function FileImagePreviewInput({
 
 		const reader = new FileReader();
 		reader.onload = () => {
-			const image = reader.result?.toString();
-			setImagePreview(image);
+			setImagePreview(reader.result?.toString());
+			setImageCropped(false);
 		};
 		reader.readAsDataURL(file);
 	};
+
+	const onImageLoad = (event: React.SyntheticEvent<HTMLImageElement>) => {
+		const { naturalWidth: width, naturalHeight: height } = event.currentTarget;
+
+		const crop = centerCrop(
+			makeAspectCrop({ unit: '%', width: 25 }, 1, width, height),
+			width,
+			height
+		);
+
+		setCrop(crop);
+	};
+
+	const cropImage = () => {
+		if (!cropperRef.current) return;
+
+		setCanvasPreview(
+			imageRef.current,
+			canvasRef.current,
+			convertToPixelCrop(crop, imageRef.current.width, imageRef.current.height)
+		);
+
+		setImagePreview(canvasRef.current.toDataURL().toString());
+		setImageCropped(true);
+		cropperRef.current.close();
+	};
+
+	useEffect(() => {
+		if (!cropperRef.current || imageCropped) return;
+
+		cropperRef.current.showModal();
+	}, [imagePreview]);
 
 	return (
 		<>
@@ -36,30 +82,52 @@ export default function FileImagePreviewInput({
 				accept='image/*'
 				onChange={imageSelectionHandler}
 			/>
-			<AnimatePresence>
-				{imagePreview.length > 0 ? (
-					<motion.img
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						exit={{ opacity: 0 }}
-						transition={{ duration: 0.15, ease: 'easeOut' }}
+			{imagePreview.length > 0 ? (
+				imageCropped ? (
+					<img
 						src={imagePreview}
 						alt='image'
 						className='w-96 mt-3 inline-block border-2 border-accent rounded-full'
 					/>
 				) : (
-					<motion.span
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						exit={{ opacity: 0 }}
-						transition={{ duration: 0.15, ease: 'easeOut' }}
-						className='mt-3 px-4 py-2 inline-block border-2 border-accent rounded-xl font-medium text-sm text-muted leading-none tracking-tighter'
+					<dialog
+						ref={cropperRef}
+						className='max-w-7xl justify-self-center top-1/2 transform -translate-y-1/2 bg-transparent overflow-hidden'
 					>
-						No image selected
-					</motion.span>
-				)}
-				/
-			</AnimatePresence>
+						<div className='flex flex-col place-content-center place-items-center gap-6'>
+							<ReactCrop
+								crop={crop}
+								onChange={(_crop, percentageCrop) => setCrop(percentageCrop)}
+								aspect={1}
+								minWidth={128}
+								circularCrop
+								keepSelection
+								className='block drop-shadow-2xl'
+							>
+								<img
+									ref={imageRef}
+									src={imagePreview}
+									alt='image'
+									className='w-full'
+									style={{ maxHeight: '75vh' }}
+									onLoad={onImageLoad}
+								/>
+							</ReactCrop>
+							<canvas ref={canvasRef} className='hidden' />
+							<button
+								onClick={() => cropImage()}
+								className='w-24 py-3 font-medium text-base text-white leading-none tracking-tighter border-2 border-white rounded-md hover:text-black hover:bg-white transition-colors duration-150'
+							>
+								Crop Image
+							</button>
+						</div>
+					</dialog>
+				)
+			) : (
+				<span className='mt-3 px-4 py-2 inline-block border-2 border-accent rounded-xl font-medium text-sm text-muted leading-none tracking-tighter'>
+					No image selected
+				</span>
+			)}
 		</>
 	);
 }
